cf80141d57da92a636f1963504f476bb04ebd136
ccubenetvix-tech <ccubenetvix@gmail.com>
2025-11-16 00:18:12 +0530

    small chnage

--- src/pages/VendorLogin.tsx
+++ src/pages/VendorLogin.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { Store, Eye, EyeOff, Mail, Lock, ArrowLeft, TrendingUp, Users } from "lucide-react";
